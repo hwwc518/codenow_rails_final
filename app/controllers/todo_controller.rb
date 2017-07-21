f54620d1a2ad9f9086b5d7b99d9fbca5f8683e31
Hui Wah Chiang <hwwc518@gmail.com>
2017-07-21 10:00:31 +0000

    installed postgres, updated database.yml and gemfile

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,17 @@ class TodoController < ApplicationController
   def index
   end
   def show
-    @todo_description = "Make the curriculum"
-    @todo_pomodoro_estimate = 4
+    todo_id = params[:id]
+    
+    if todo_id == '1' 
+      @todo_description = "Make the curriculum"
+      @todo_pomodoro_estimate = 4
+    elsif todo_id == '2'
+      @todo_description = "Buy workshop supplies"
+      @todo_pomodoro_estimate = 3
+    elsif todo_id == '3'
+      @todo_description = "Make food"
+      @todo_pomodoro_estimate = 2
+  end
   end
 end
