1069e69ce8bc393ae604e8f10dae80fdc8b0c4e6
Hui Wah Chiang <hwwc518@gmail.com>
2017-07-21 11:58:07 +0000

    added create route

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,17 +2,9 @@ class TodoController < ApplicationController
   def index
   end
   def show
-    todo_id = params[:id]
-    
-    if todo_id == '1' 
-      @todo_description = "Make the curriculum"
-      @todo_pomodoro_estimate = 4
-    elsif todo_id == '2'
-      @todo_description = "Buy workshop supplies"
-      @todo_pomodoro_estimate = 3
-    elsif todo_id == '3'
-      @todo_description = "Make food"
-      @todo_pomodoro_estimate = 2
+    @todo = Todo.find_by_id(params[:id])
   end
+  def create
   end
+  
 end
